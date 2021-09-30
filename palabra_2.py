971bfb9060eb25ca467797263443ea76c934a643
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-09-30 08:44:18 +0200

    Add files via upload

---
+++ palabra_2.py
@@ -0,0 +1,17 @@
+def palabra_2():
+    nombre=raw_input("NOMBRE: ")
+    apellido=raw_input("APELLIDO: ")
+    print("Buenos dias, "+nombre+" "+apellido)
+    print("Tu lindo nombre empieza por la letra "+nombre[0])
+    print("Voy a deletrear tu nombre")
+    print(nombre[0])
+    print(nombre[1])
+    print(nombre[2])
+    print(nombre[3])
+    print(nombre[4])
+    print(nombre[5])
+    print(nombre[6])
+    print(nombre[7])
+    print(nombre[8])
+
+palabra_2()
