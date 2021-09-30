7b3575effe126cf963d74144a9fb0e67b8ed6fa8
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-09-30 08:54:06 +0200

    Add files via upload

---
+++ palabra_3.py
@@ -0,0 +1,9 @@
+def palabra_3():
+    nombre=raw_input("NOMBRE: ")
+    print("Buenos dias, "+nombre)
+    print("Tu lindo nombre empieza por la letra "+nombre[0])
+    print("Voy a deletrear tu nombre")
+    for cont in range(0,20):
+        print(nombre[cont])
+
+palabra_3()
