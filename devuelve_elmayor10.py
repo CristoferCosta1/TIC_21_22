9f020ea6582f9b8c7cc0bdc4afec0e2951d246cc
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-11-15 10:34:31 +0100

    Add files via upload

---
+++ devuelve_elmayor10.py
@@ -0,0 +1,9 @@
+def devuelve_el_mayor10():
+    mayor=input("Dime un numero: ")
+    for cont in range(1,10):
+        num=input("Dime un numero: ")
+        if(num>mayor):
+            mayor=num
+    print("MAYOR= "+str(mayor))
+devuelve_el_mayor10()
+    
