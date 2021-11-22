c56b8612cf3db4fd8a70a14a3e9e40b1a1d23dab
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-11-22 10:30:15 +0100

    Add files via upload

---
+++ piramide.py
@@ -0,0 +1,34 @@
+def piramide():
+    fila_completa=" "
+    n=input("Dame un numero: ")
+    for fil in range(n,0,-1):
+        for col in range(1,fil+1):
+            fila_completa=fila_completa+str(fil)
+        print fila_completa
+        fila_completa=" "
+            
+
+piramide()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
