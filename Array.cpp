7b7e437ebde2403224a2f86147b46dc06812a90b
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-12-09 08:47:16 +0100

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	int x[10];
+	int cont;
+	//for cont in range(1,10);
+	for(cont=1;cont<=10;cont=cont++){
+		//num=input("Dame un numero: ")
+		printf("Dame un numero: ");
+		scanf("%d",&x[cont]);
+		
+	}
+	return(0);
+	
+	
+	
+}
