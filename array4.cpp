088d04ebd3aa525a96147d3fe00a6bb03f2b62cb
CristoferCosta1 <90762325+CristoferCosta1@users.noreply.github.com>
2021-12-16 08:58:16 +0100

    Add files via upload

---
+++ array4.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+	char letras[5];
+	int cont;
+
+	
+	//Leo datos del teclado
+	for(cont=0;cont<5;cont++){
+		//num=input("Dame un numero: ")
+		printf("Introduce la letra %d: ",cont);
+		scanf(" %c",&letras[cont]);
+		
+	}
+	//ESCRIBO DATOS EN LA PANTALLA
+	printf("\nHAS ESCRITO LA PALABRA: ");
+	for(cont=0;cont<5;cont++){
+		printf("%c",letras[cont]);
+
+
+	}
+	printf("\n");
+	//ESCRIBO LA PALABRA AL REVÉS
+	for (cont=4;cont>=0;cont--){
+		printf("%c",letras[cont]);
+		
+	}
+
+	
+	return(0);
+	
+	
+	
+}
